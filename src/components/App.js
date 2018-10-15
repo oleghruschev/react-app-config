426b087f91fbe62c4bab59f4553b8e2c6f6d4151
Oleg <hruschev.oleg@mail.ru>
2018-10-15 22:08:46 +0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => (
+	<div className="block">
+		<div className="block__img"/>
+		<h1 className="block__header">Hello</h1>
+	</div>	
+)
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require ('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const HtmlPlugin = new HtmlWebPackPlugin ({
+  template: './index.html'
+});
+const MiniCssPlugin = new MiniCssExtractPlugin({
+	filename: './style.css',
+});
+const HotModulePlugin = new webpack.HotModuleReplacementPlugin();
+
+
+module.exports = {
+	// context: path.resolve(__dirname, 'src'),
+
+	entry: './src/index.js',
+
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/'
+	},
+	//Настройки локального сервера
+	devServer: {
+    contentBase: './build',
+    hot: true
+  },
+
+	module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+      	test: /\.scss$/,
+      	use: [
+      		MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              // modules: true,
+              // importLoaders: 1,
+              // localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true
+              // minimize: true
+            }
+          },
+          {
+						loader: 'sass-loader',
+						options: { sourceMap: true },
+					}
+      	]
+      },
+      //загрузка изображений
+			{
+				test: /\.(png|gif|jpe?g)$/,
+				loaders: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'images/[name].[ext]',
+						}
+					},
+					'img-loader',
+				]
+			},
+    ]
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  plugins: [
+  	HtmlPlugin,
+  	MiniCssPlugin,
+    HotModulePlugin,
+  ],
+}
