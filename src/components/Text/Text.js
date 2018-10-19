765107d166c6452d286efe31f417f37456f4f65b
Oleg <32393175+oleghruschev@users.noreply.github.com>
2018-10-19 15:06:10 +0500

    Update Text.js

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Text.scss';
 
 const Text = () => (
-  <p className={styles.paragraph}>This is my demonstration project</p>
+  <p className={styles.paragraph}>This is my react-app-configuration</p>
 )
 
 export default Text;
